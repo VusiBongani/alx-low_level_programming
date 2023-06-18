ed35c838f3f30ec6f5eb3f7a1a16d12e3379d200
VusiBongani <sonofman012@gmail.com>
2023-06-17 17:58:11 -0700

    Unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Printing
+ *
+ * Return: always 1 (success)
+ */
+
+int main(void)
+{
+	write(2, "nd that piece of art is useful\"-Dora Korpat, 2015-10-19\n", 59);
+	return (1);
+}
+
