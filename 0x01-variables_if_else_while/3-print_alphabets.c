30edb849394b3e23746e7cfe21ba56923d742c08
VusiBongani <sonofman012@gmail.com>
2023-06-19 19:20:35 -0700

    Lower and Upper case

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,8 +8,18 @@
 int main(void)
 {
 	char Up_case;
+	char L_case;
+
+	for (L_case = 'a'; L_case <= 'z'; L_case++)
+	{
+		putchar(L_case);
+	}
 
 	for (Up_case = 'A'; Up_case <= 'Z'; Up_case++)
+	{
 		putchar(Up_case);
+	}
+		putchar('\n');
+
 	return (0);
 }
