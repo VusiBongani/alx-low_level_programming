2e344e0eec53223572f1179c7fdc67c216c4a8bb
VusiBongani <sonofman012@gmail.com>
2023-06-19 22:15:16 -0700

    Print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * description: printing numbers less than 10 using SCICode
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+	int p;
+
+	for (p = 0; p < 10; p++)
+	{
+		putchar (48 + n);
+		n++;
+	}
+
+	putchar('\n');
+	return (0);
+}
