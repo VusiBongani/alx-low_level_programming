d924919fe187e6cfd33b092acd9ce592ac1fd187
VusiBongani <sonofman012@gmail.com>
2023-06-19 18:18:03 -0700

    Lowercase Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - loops
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	char L_case;
+
+	for (L_case = 'a'; L_case <= 'z'; L_case++)
+		putchar(L_case);
+	return (0);
+}
