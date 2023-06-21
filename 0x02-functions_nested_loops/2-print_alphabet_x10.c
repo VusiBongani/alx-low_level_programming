0311b8f913b45e8c9cc603e7387e5b620505634b
VusiBongani <sonofman012@gmail.com>
2023-06-20 21:32:07 -0700

    Printing alphabets 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints alphabet
+ * print_alphabet_x10 - prints alphabet
  * description: print out alphabet in lowercase 10x
  * Return: void
  */
@@ -11,7 +11,7 @@ void print_alphabet_x10(void)
 	char L_case;
 	int a = 0;
 
-	while (a < 0)
+	while (a < 10)
 	{
 		for (L_case = 'a'; L_case <= 'z'; L_case++)
 		{
