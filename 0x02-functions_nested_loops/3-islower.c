bf5dd885d77d1b45f5632c8baf8cd7cfd849c4e6
VusiBongani <sonofman012@gmail.com>
2023-06-21 13:28:51 -0700

    Lower and Upper case checks

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.c"
+
+/**
+ * _islower - checks the lower and upper case
+ * @b: parameter
+ * Return: 1 and 0
+ */
+
+int _islower(int b)
+{
+	if (b >= 97 && b <= 122)
+	{
+		return (1);
+	}
+	else if (b >= 65 && b <= 90)
+	{
+		return (0);
+	}
+}
