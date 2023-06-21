3148e84c0f7ad190f64341d4c9f9fd1477e8077e
VusiBongani <sonofman012@gmail.com>
2023-06-21 14:40:53 -0700

    Upper and Lower case

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - checks the lower and upper case
+ * @c: parameter
+ * Return: 1 and 0
+ */
+
+int _isalpha(int c);
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else 
+		return (0);
+}
+
