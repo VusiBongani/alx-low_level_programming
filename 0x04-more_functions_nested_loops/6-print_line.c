d8ec48dfa191055ec6ca6df2241efcb1cc89df1c
VusiBongani <sonofman012@gmail.com>
2023-06-22 16:55:39 -0700

    plot a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - Draw a straight line on the terminal
+ * @c: int value
+ * Return: void 
+ */
+
+void print_line(int n)
+{
+	int b;
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (b = 1; b <= n; b++)
+			_putchar('_');
+		_putchar('\n');
+	}
+}
