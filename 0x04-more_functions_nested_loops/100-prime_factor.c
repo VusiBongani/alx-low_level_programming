97c31026caca10ee7522105f49022b8b63ecff55
VusiBongani <sonofman012@gmail.com>
2023-06-22 20:22:49 -0700

    Printing Squatres

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_square - print a square
+ * @size: size of squares
+ * Return: always 0 (success)
+ */
+
+void print_square(int size)
+{
+	int a;
+	int b;
+
+	for (a = 1; row <= size; a++)
+	{
+		for (b = 1; b <= size; b++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
