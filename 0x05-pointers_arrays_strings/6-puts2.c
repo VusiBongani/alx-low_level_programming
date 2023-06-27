b0aa153adffad833b5a5beb72a7f7e423496fc39
VusiBongani <sonofman012@gmail.com>
2023-06-26 20:43:06 -0700

    Prints characters

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints characters of A STRING
+ * @str:string parameter
+ * Return:void
+ */
+
+void puts2(char *str)
+{
+	int i;
+	int j = 0;
+
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+
+	for (i = 0; i < j; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
