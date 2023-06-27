b3ec5ee9f8166cbaf1e94621bd67debbc7ed610e
VusiBongani <sonofman012@gmail.com>
2023-06-26 17:57:19 -0700

    Reset a pointer value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update the value of a point
+ * @n: int pointer
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	int *p;
+
+	p = n;
+
+	*p = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Learning the use of pointers, arrays and strings
