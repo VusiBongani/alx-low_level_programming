0adf555f2f72936dc58ffa081a5b1cf89a839356
VusiBongani <sonofman012@gmail.com>
2023-06-29 20:37:17 -0700

    capitalizing

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * *cap_string - capitalizes all words of a string
+ * @CAPS: char parameter
+ * Return: characters
+ */
+
+char *cap_string(char *CAPS)
+{
+	char *ptr = CAPS;
+	int capital = 1;
+
+	while (*ptr != '/0')
+	{
+		if (*ptr == ' ' || *ptr == '\t' || *ptr == '\n' || *ptr == ',' ||
+				*ptr == ';' || *ptr == '.' || *ptr == '!' ||
+				*ptr == '?' || *ptr == '\"' || *ptr == '(' ||
+				*ptr == ')' || *ptr == '{' || *ptr == '}')
+		{
+			capital = 1;
+		}
+		else if (isalpha(*ptr))
+		{
+			if (capital)
+			{
+				*ptr = toupper(*ptr);
+				capital = 0;
+			}
+			else
+			{
+				*ptr = tolower(*ptr);
+			}
+		}
+
+		ptr++;
+	}
+
+	return (CAPS);
+}
