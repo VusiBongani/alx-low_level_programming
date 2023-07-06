7be85fba8f0547e6eb9b95ce5299513c370496d9
VusiBongani <sonofman012@gmail.com>
2023-07-05 19:28:19 -0700

    Trial number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,11 +2,36 @@
 
 /**
  * is_prime_number - checks the divisor
- * @n: integer
+ * @n: intege
+ * @divisor: divisor
  * Return: integer
  */
 
+
+int is_prime_helper(int n, int divisor)
+{
+	if (divisor * divisor > n)
+	{
+		return (1);
+	}
+	if (n % divisor == 0)
+	{
+		return (0);
+	}
+
+	return (is_prime_helper(n, divisor + 1));
+}
+
 int is_prime_number(int n)
 {
-	return (is_prime_recursive(n, 5));
+	if (n <= 1)
+	{
+		return (0);
+	}
+	if (n == 2)
+	{
+		return (1);
+	}
+
+	return (is_prime_helper(n, 2));
 }
